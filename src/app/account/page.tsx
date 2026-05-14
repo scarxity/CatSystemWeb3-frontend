d5d1f0f35a42b4b1e0957bc5ea37e6b364c0121d
vinsa61 <vinsarioshen610@gmail.com>
2026-05-14 15:53:51 +0700

    fix: remove conflicting pages router

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -2,14 +2,14 @@
 
 import withAuth from "@/components/hoc/withAuth";
 import Layout from "@/layouts/Layout";
-import AccountPage from "@/pages/account/AccountPage";
+import AccountPage from "@/views/account/AccountPage";
 
 function Page() {
-	return (
-		<Layout withNavbar>
-			<AccountPage />
-		</Layout>
-	);
+  return (
+    <Layout withNavbar>
+      <AccountPage />
+    </Layout>
+  );
 }
 
 export default withAuth(Page, "auth");
